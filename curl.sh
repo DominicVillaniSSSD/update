36957f68e30e4e21af6197d82f54979dbc5956c4
Dominic Villani <dvillani@sssd.k12.ca.us>
2024-05-30 13:23:54 -0700

    Update curl.sh
    added crisis go as a var to be installed

--- curl.sh
+++ curl.sh
@@ -8,6 +8,7 @@ smart_notebook_url="https://downloads.smarttech.com/software/education/23.2/mac/
 zoom_url="https://zoom.us/client/latest/Zoom.pkg"
 air_server_url="https://dl.airserver.com/mac/AirServer-7.2.7.dmg"
 app_cleaner_url="https://freemacsoft.net/downloads/AppCleaner_3.6.8.zip"
+crisis_go="https://crisisgoapp.s3.amazonaws.com/Mac/CrisisGo_6.22.1.pkg"
 
 
 install_application_from_url() {
