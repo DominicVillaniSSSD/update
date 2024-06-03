994cf57ea2f45728c8e7a5874932f092a087c8ec
Dominic Villani <dvillani@sssd.k12.ca.us>
2024-06-03 10:29:06 -0700

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -4,7 +4,7 @@ source install_handlers.sh
 
 google_chrome_url="https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg"
 google_drive_url="https://dl.google.com/drive-file-stream/GoogleDrive.dmg"
-smart_notebook_url="https://downloads.smarttech.com/software/education/23.2/mac/23.2.278.0/smart23-2-web.dmg"
+smart_notebook_url="https://downloads.smarttech.com/software/education/24.0/mac/24.0.238.0/smart24-0-web.dmg"
 zoom_url="https://zoom.us/client/latest/Zoom.pkg"
 air_server_url="https://dl.airserver.com/mac/AirServer-7.2.7.dmg"
 app_cleaner_url="https://freemacsoft.net/downloads/AppCleaner_3.6.8.zip"
