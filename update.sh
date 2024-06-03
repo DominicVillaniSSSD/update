ef47ce49a35c346245dc6080d952b775e4395fa7
Dominic Villani <dvillani@sssd.k12.ca.us>
2024-06-03 14:27:52 -0700

    doing merge manually

--- update.sh
+++ update.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # Directory to store the scripts
+
 TEMP_DIR="/tmp/update-scripts"
 
 # Create a temporary directory
 mkdir -p $TEMP_DIR
 cd $TEMP_DIR
 
+# Set the branch
+branch="main"
+
 # Download necessary scripts
-curl -L -o curl.sh https://raw.githubusercontent.com/DominicVillaniSSSD/update/main/curl.sh
-curl -L -o install_handlers.sh https://raw.githubusercontent.com/DominicVillaniSSSD/update/main/install_handlers.sh
-curl -L -o logo.sh https://raw.githubusercontent.com/DominicVillaniSSSD/update/main/logo.sh
-curl -L -o setup.sh https://raw.githubusercontent.com/DominicVillaniSSSD/update/main/setup.sh
+curl -L -o curl.sh https://raw.githubusercontent.com/DominicVillaniSSSD/update/$branch/curl.sh
+curl -L -o install_handlers.sh https://raw.githubusercontent.com/DominicVillaniSSSD/update/$branch/install_handlers.sh
+curl -L -o logo.sh https://raw.githubusercontent.com/DominicVillaniSSSD/update/$branch/logo.sh
+curl -L -o setup.sh https://raw.githubusercontent.com/DominicVillaniSSSD/update/$branch/setup.sh
 # Add more curl commands for additional scripts as needed
 
 # Make the scripts executable
@@ -30,24 +34,28 @@ source ./setup.sh
 
 #Prints logo
 print_logo
-
+echo "this is the $branch branch"
 #Checks architecture
 check_architecture
 
-#if [[ "$ARCH" == "arm64" ]]; then
-#    zoom_url="$zoom_apple_silicon_url"
-#fi
+#sets zoom_url to zoom_arm64_url if cpu is arm otherwise it will keep intell link
+ if [[ "$ARCH" == "arm64" ]]; then
+     zoom_url="$zoom_arm64_url"
+ fi
+
 
 install_applications(){
+install_application_from_url "$app_cleaner_url"
 install_application_from_url "$zoom_url"
 install_application_from_url "$smart_notebook_url"
 install_application_from_url "$google_chrome_url"
-install_application_from_url "$app_cleaner_url"
 install_application_from_url "$air_server_url"
 install_application_from_url "$crisis_go"
 }
 
 install_applications
+
+print_finished
 
 # Clean up
 cd ..
