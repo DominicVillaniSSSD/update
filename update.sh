869eb070acd19eb3ecff509a2cbde84d2836c7f9
Dominic Villani <dvillani@sssd.k12.ca.us>
2024-06-03 10:34:42 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -50,13 +50,11 @@ install_application_from_url "$google_chrome_url"
 install_application_from_url "$air_server_url"
 install_application_from_url "$crisis_go"
 }
-install_application_from_url "$smart_notebook_url"
-#install_applications
+#install_application_from_url "$smart_notebook_url"
+install_applications
+
 print_finished
 
-#install_application_from_url "$zoom_url"
 # Clean up
-print_finished
-
 cd ..
 rm -rf $TEMP_DIR
